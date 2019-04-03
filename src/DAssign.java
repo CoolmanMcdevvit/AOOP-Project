838eed773b1813f248bb1293880c35afcf731177
mariux255 <44610785+mariux255@users.noreply.github.com>
2019-04-03 16:09:40 +0200

    !Test! Class to do admission.

--- src/DAssign.java
+++ src/DAssign.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class DAssign {
-}
+    public static void Init(){
+        System.out.println("Welcome to Admission Control Server");
+        System.out.println("Which Department is the patient in?");
+        System.out.println("1 - Emergency");
+        System.out.println("2 - Inpatient");
+        System.out.println("3 - Outpatient");
+        Scanner department = new Scanner(System.in);
+        if (department.equals(1))
+            //Emergency class stuff
+            System.out.println("Emergency department chosen");
+        if (department.equals(2))
+            /*Inpatient class stuff
+            Which means a bed is given for a patient*/
+            System.out.println("Inpatient department chosen");
+        if (department.equals(3))
+            //Outpatient class stuff
+            System.out.println("Outpatient department chosen");
+}}
