02234e4bb5c9418f4aa93a7fdbc746a8149aa5ac
Jack <Jwmrose@live.com>
2019-05-02 10:56:13 +0200

    quick push

--- src/GUI/Model/Homepage.java
+++ src/GUI/Model/Homepage.java
@@ -4,7 +4,7 @@ public class Homepage {
 
 public String patientRegModel(String n, String sn, String bd, String ha,String pn, String t, String ad){
         if ((ad.toLowerCase().equals("alive") || ad.toLowerCase().equals("dead") && pn.length() == 8)) {
-//            Main.patientSignUp(n,sn,bd,ha,pn,t,ad);
+            Main.patientSignUp(n,sn,bd,ha,pn,t,ad);
             return "Success";
         }
         else{
