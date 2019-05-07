b6749622e81e7b53e75e1fd7417cffd37fd0078a
ClayyyZ <40458081+claytonycm@users.noreply.github.com>
2019-05-07 20:27:12 +0200

    updated sql

--- hospital_java.sql
+++ hospital_java.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS Staff;
 
 CREATE TABLE Facility
 	(Fac_Name		ENUM("emergency", "inpatient", "outpatient"),
-	 Fac_Bed	INT,
+	 Fac_Bed		INT,
 	 PRIMARY KEY(Fac_Name)
 	);
     
@@ -16,10 +16,11 @@ CREATE TABLE Staff
 	(St_SerialNum	INT auto_increment, #auto
 	 St_Name		VARCHAR(20),
 	 St_Surname		VARCHAR(40), 
-     St_Role		ENUM("Doctor", "Nurse", "Clerk", "ICT officer"),
+     St_Role		VARCHAR(20),#ENUM("Doctor", "Nurse", "Clerk", "ICT officer"),
      #not specificed for Clerk and ICT
-     St_Dept		ENUM("emergency", "inpatient", "outpatient", "not specificed"),
-     St_Email		VARCHAR(100) GENERATED ALWAYS AS (CONCAT(St_Name,'_',St_Surname,'@hos.com')), #auto
+     St_Dept		VARCHAR(20),
+     St_Phone		VARCHAR(12),
+     St_Email		VARCHAR(100), #GENERATED ALWAYS AS (CONCAT(St_Name,'_',St_Surname,'@hos.com')), #auto
 	 PRIMARY KEY(St_SerialNum)
 	);
 
@@ -29,26 +30,25 @@ CREATE TABLE Patient
 	 Pati_Surname	VARCHAR(40),
 	 Pati_Bdate		VARCHAR(10),	#dd.mm.yyyy
      Pati_Address	VARCHAR(400),
-     Pati_Dept		ENUM("emergency", "inpatient", "outpatient", "not assigned"),
-	 Pati_Phone		VARCHAR(12),
-	 Pati_Tribe		ENUM("Asia", "US", "EU", "Africa", "Australia", "Others"),
-     Pati_Alive		ENUM("alive","dead"),
+     Pati_Dept		VARCHAR(20),
+	 Pati_Phone		INT,
+	 Pati_Tribe		VARCHAR(20),
+     Pati_Alive		BOOLEAN,
+     Pati_Bed_num	INT,
 	 PRIMARY KEY(Pati_ID)
      #FOREIGN KEY(Pati_Dept) REFERENCES Facility(Fac_Name) ON DELETE SET NULL
 	);
 
-INSERT INTO Patient (Pati_Name, Pati_Surname, Pati_Bdate, Pati_Address, Pati_Phone,Pati_Tribe, Pati_Dept)
-VALUES( "Ken","simon","31.2.1232","ds32ds","+23232321","Asia",2),
-( "KK","sim","31.3.1232","ddgd46ds","+65232321","Asia",2),
-( "Sam","samson","43.4.4343","dsds4353","+31231234","US",1),
-( "BB","sim","31.4.2332","dfsafadgd46ds","+265232321","US",3);
+INSERT INTO Patient (Pati_ID, Pati_Name, Pati_Surname, Pati_Bdate, Pati_Address, Pati_Phone,Pati_Tribe, Pati_Dept, Pati_Bed_num)
+VALUES("10001", "Ken","simon","03.12.1232","ds32ds","23232321","Asia",2, 213),
+("10002", "KK","sim","13.03.1232","ddgd46ds","65232321","Asia",2, 214),
+("10003", "Sam","samson","13.04.4343","dsds4353","31231234","US",1, 122),
+("10004","BB","sim","11.04.2332","dfsafadgd46ds","65232321","US",3, 3);
 
-INSERT INTO Staff (St_Name, St_Surname, St_Role, St_Dept)
-VALUES("Byan","Sean",2,1);
+INSERT INTO Staff (St_Name, St_Surname, St_Role)
+VALUES("Byan","Sean","wtf");
 
 INSERT INTO Facility (Fac_Name, Fac_Bed)
 VALUES(1,200),
 (2,300),
 (3,400);
-
-UPDATE Staff SET St_Name = 'jj' WHERE St_SerialNum = 1
